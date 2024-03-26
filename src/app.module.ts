eef4510558f1146b7e6473d675bbc392079c249e
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2024-03-26 04:26:19 -0400

    create raffles controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RafflesController } from './raffles/raffles.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RafflesController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/raffles/raffles.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('raffles')
+export class RafflesController {}
