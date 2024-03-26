756c7ff519d4cd71d6cf76b74f287a9d7918d4db
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2024-03-26 12:37:19 -0400

    update

--- src/raffles/raffles.controller.ts
+++ src/raffles/raffles.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('raffles')
-export class RafflesController {}
+export class RafflesController {
+  @Get()
+  findAll() {
+    return `This action returns all raffle`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns ${id} raffle`;
+  }
+}
