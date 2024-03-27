79ff00bba5ccc8daf7046f57a2c384ac2c80b1b7
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2024-03-27 07:59:18 -0400

    basic crud for raffle service

--- src/raffles/raffles.service.ts
+++ src/raffles/raffles.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class RafflesService {}
+export class RafflesService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createRaffeDto: Prisma.RaffleCreateInput) {
+    return this.databaseService.raffle.create({ data: createRaffeDto });
+  }
+
+  async findAll() {
+    return this.databaseService.raffle.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.raffle.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateRaffleDto: Prisma.RaffleUpdateInput) {
+    return this.databaseService.raffle.update({
+      where: {
+        id,
+      },
+      data: updateRaffleDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.databaseService.raffle.delete({
+      where: { id },
+    });
+  }
+}
